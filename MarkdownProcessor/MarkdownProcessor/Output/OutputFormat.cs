e64c6da63094a39d040241a05fb4ee9703befde7
Nathan Jervis <mirhagk@gmail.com>
2013-12-29 19:20:32 -0500

    Added output format class
    Contains helper methods to compile to many different outputs (streams,
    files, or strings) without child classes worrying about it. Will also
    contain helper methods for getting the before and after parts of the
    file

---
+++ MarkdownProcessor/MarkdownProcessor/Output/OutputFormat.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarkdownProcessor.Output
+{
+    public abstract class OutputFormat
+    {
+        protected abstract void CompileToStream(System.IO.StreamWriter stream, List<Nodes.Node> nodes);
+        public void CompileToStream(System.IO.Stream stream, List<Nodes.Node> nodes)
+        {
+            var writer = new System.IO.StreamWriter(stream);
+            CompileToStream(writer, nodes);
+        }
+        public string CompileToString(List<Nodes.Node> nodes)
+        {
+            var stream = new System.IO.MemoryStream();
+            var reader = new System.IO.StreamReader(stream);
+            CompileToStream(stream, nodes);
+            return reader.ReadToEnd();
+        }
+        public void CompileToFile(string fileName, List<Nodes.Node> nodes)
+        {
+            var file = new System.IO.StreamWriter(fileName);
+            CompileToStream(file, nodes);
+            file.Close();
+        }
+    }
+}
