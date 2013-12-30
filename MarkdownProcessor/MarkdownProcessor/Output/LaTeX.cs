069ca612e5bf98014504b21f518ad1a6a12118d8
Nathan Jervis <mirhagk@gmail.com>
2013-12-29 19:20:53 -0500

    Added LaTeX output format
    No implementation yet

---
+++ MarkdownProcessor/MarkdownProcessor/Output/LaTeX.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarkdownProcessor.Output
+{
+    class LaTeX : OutputFormat
+    {
+        protected override void CompileToStream(System.IO.StreamWriter stream, List<Nodes.Node> nodes)
+        {
+            foreach (var node in nodes)
+            {
+            }
+            throw new NotImplementedException();
+        }
+    }
+}
