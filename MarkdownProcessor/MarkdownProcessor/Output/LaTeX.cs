bc6895bed3067ff772a1bd91c2359418242a6bd6
Nathan Jervis <mirhagk@gmail.com>
2014-01-01 12:01:13 -0500

    Automatically get the wrapper text for the output

--- MarkdownProcessor/MarkdownProcessor/Output/LaTeX.cs
+++ MarkdownProcessor/MarkdownProcessor/Output/LaTeX.cs
@@ -8,7 +8,7 @@ namespace MarkdownProcessor.Output
 {
     class LaTeX : OutputFormat
     {
-        protected override void CompileToStream(System.IO.StreamWriter stream, List<Nodes.Node> nodes)
+        protected override void Compile(System.IO.StreamWriter stream, List<Nodes.Node> nodes)
         {
             foreach (var node in nodes)
             {
--- MarkdownProcessor/MarkdownProcessor/Output/OutputFormat.cs
+++ MarkdownProcessor/MarkdownProcessor/Output/OutputFormat.cs
@@ -8,11 +8,23 @@ namespace MarkdownProcessor.Output
 {
     public abstract class OutputFormat
     {
-        protected abstract void CompileToStream(System.IO.StreamWriter stream, List<Nodes.Node> nodes);
+        protected abstract void Compile(System.IO.StreamWriter stream, List<Nodes.Node> nodes);
+        private string GetStart()
+        {
+            string fileName = String.Format("Templates/{0}_start.txt", this.GetType().Name);
+            return System.IO.File.ReadAllText(fileName);
+        }
+        private string GetEnd()
+        {
+            string fileName = String.Format("Templates/{0}_end.txt", this.GetType().Name);
+            return System.IO.File.ReadAllText(fileName);
+        }
         public void CompileToStream(System.IO.Stream stream, List<Nodes.Node> nodes)
         {
             var writer = new System.IO.StreamWriter(stream);
-            CompileToStream(writer, nodes);
+            writer.Write(GetStart());
+            Compile(writer, nodes);
+            writer.Write(GetEnd());
         }
         public string CompileToString(List<Nodes.Node> nodes)
         {
@@ -24,7 +36,7 @@ namespace MarkdownProcessor.Output
         public void CompileToFile(string fileName, List<Nodes.Node> nodes)
         {
             var file = new System.IO.StreamWriter(fileName);
-            CompileToStream(file, nodes);
+            CompileToStream(file.BaseStream, nodes);
             file.Close();
         }
     }
